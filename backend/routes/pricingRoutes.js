951b43998bc61d4cb8a9eb2b67daf2d84ccf14f2
Senuthii Amarasinghe <asenuthisahansa@gmail.com>
2025-10-17 06:22:43 +0530

    update pricingRoutes.js import

--- backend/routes/pricingRoutes.js
+++ backend/routes/pricingRoutes.js
@@ -1,6 +1,6 @@
 import express from "express";
 import { getQuote } from "../controllers/pricingController.js";
-import { authenticate } from "../middleware/authMiddleware.js";
+import { authenticate } from "../middlewares/authMiddleware.js";
 
 const router = express.Router();
 
